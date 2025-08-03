9c16b9a56c4df8b3a3c03f20a54fdf3e06c00b5e
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2025-08-03 11:14:11 -0400

    add documentation for vec2 and wall

--- src/vec.h
+++ src/vec.h
@@ -11,12 +11,38 @@ struct vec2 {
     vec2 operator*(const float& scalar) const;
     vec2 operator/(const float& scalar) const;
 
+    /**
+     * Compute the dot product between two vectors.
+     * @param other the other vector
+     * @return the dot product
+     */
     [[nodiscard]] float dot(const vec2& other) const;
 
+    /**
+     * Distance between two points.
+     * @param other the other point
+     * @return the Euclidean distance
+     */
     [[nodiscard]] float dist(const vec2& other) const;
+
+    /**
+     * Vector length
+     * @return this vector's length
+     */
     [[nodiscard]] float length() const;
+
+    /**
+     * Normalized vector
+     * @return the normalized (length 1) version of this vector
+     */
     [[nodiscard]] vec2 normalize() const;
 
+    /**
+     * Print out vector (<x, y>)
+     * @param os ostream to print to
+     * @param vec vector to print
+     * @return the passed in ostream
+     */
     friend std::ostream& operator<<(std::ostream& os, const vec2& vec);
 };
 
--- src/wall.h
+++ src/wall.h
@@ -13,9 +13,33 @@ struct line {
 class wall {
 public:
     wall(vec2 pos, vec2 direction);
+
+    /**
+     * Wall's (starting) position
+     * @return the position
+     */
     [[nodiscard]] vec2 get_pos() const;
+
+    /**
+     * Wall's direction vector, which can be used to
+     * compute the ending position of the wall.
+     *
+     * @return the direction vector
+     */
     [[nodiscard]] vec2 get_direction() const;
+
+    /**
+     * Get the line representation of the wall
+     * @return line representation
+     */
     [[nodiscard]] line get_line() const;
+
+    /**
+     * Normal vector of the wall -- a vector which is
+     * perpendicular to the wall.
+     *
+     * @return a normal vector
+     */
     [[nodiscard]] vec2 get_normal() const;
 private:
     vec2 pos;
